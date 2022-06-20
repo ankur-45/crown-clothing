eae6794e8585277008214d1bbbca473c2378a3a6
ankur-45 <ankur070593@gmail.com>
2022-06-20 22:45:44 +0530

    Built cart dropdown icon

--- crown-clothing/src/components/product-card/product-card.component.jsx
+++ crown-clothing/src/components/product-card/product-card.component.jsx
@@ -1,18 +1,24 @@
-import './product-card.styles.scss';
-import Button from '../button/button.component';
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
+import "./product-card.styles.scss";
+import Button from "../button/button.component";
 
 const ProductCard = ({ product }) => {
-    const { name, price, imageUrl } = product
-    return (
-        <div className='product-card-container'>
-            <img src={imageUrl} alt={`${name}`} />
-            <div className='footer'>
-                <span className='name'>{name}</span>
-                <span className='price'>{price}</span>
-            </div>
-            <Button>Add To Cart</Button>
-        </div>
-    )
-}
+  const { name, price, imageUrl } = product;
+  const { addItemsToCart } = useContext(CartContext);
+  const addProductToCart = () => addItemsToCart(product);
+  return (
+    <div className="product-card-container">
+      <img src={imageUrl} alt={`${name}`} />
+      <div className="footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <Button buttonType="inverted" onClick={addProductToCart}>
+        Add To Cart
+      </Button>
+    </div>
+  );
+};
 
 export default ProductCard;
